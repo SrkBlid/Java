fce441d0440178b8cea353ead35e1385adecc3a1
SrkBlid <mezzanojoaquin@gmail.com>
2023-06-19 14:06:22 -0300

    Estructuras principale (Hasta operadores)

---
+++ ProyectoJava/src/EstructurasPrincipales.java
@@ -0,0 +1,32 @@
+public class EstructurasPrincipales {
+    public static void main(String[] args){
+        //Tipo string, char
+        System.out.println("Hello, World!");
+        //Tipo int, short (-32.768 a 32.767), long (Sufijo L), byte (-128 a 127)
+        System.out.println(5);
+        //Tipo float (Sufijo F), double
+        System.out.println(3.25F);
+        //Tipo booleano
+        //Variables en java
+        boolean activo;
+        activo = true;
+        System.out.println(activo);
+        activo = false;
+        System.out.println(activo);
+        //Declarar una constante en java (con final antes del tipo de dato)
+        final float pi = 3.14F;
+
+        //OPERADORES 
+        //Lógicos (>, <, !=, ==, &&, ||)
+        //Incremento/Decremento (++, --, += n, -= n)
+        //Concatenación (+)
+        int a = 5;
+        int b = 7;
+        int c = a+b;
+        System.out.println(c);
+        c++;
+        System.out.println(c);
+        c-=a+1;
+        System.out.println(c);
+    }
+}
