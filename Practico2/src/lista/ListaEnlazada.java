6b1d95fea18b2b4543f57c616976e1e0a13eb42c
SrkBlid <mezzanojoaquin@gmail.com>
2023-09-06 20:39:24 -0300

    Finalización listas enlazadas

--- Practico2/src/lista/ListaEnlazada.java
+++ Practico2/src/lista/ListaEnlazada.java
@@ -1,27 +1,194 @@
 package lista;
 
-public class ListaEnlazada<T> implements Lista<T> {
+public class ListaEnlazada<T> implements Lista<T>{
 	
-	Object info;
-	ListaEnlazada siguiente;
+	private Object info;
+	private ListaEnlazada siguiente;
+	private int cantidad;
 	
-	//CONSTRUCTORES
-	public ListaEnlazada() {
+	ListaEnlazada() {
 		info = null;
 		siguiente = null;
+		cantidad = 0;
 	}
 	
-	public ListaEnlazada(Object item) {
-		info = item;
+	ListaEnlazada(T elem) {
+		info = elem;
 		siguiente = null;
+		cantidad++;
 	}
 	
-	public ListaEnlazada(Object item, ListaEnlazada siguiente) {
-		info = item;
-		this.siguiente = siguiente;
+	public boolean agregar(T elem) {
+		if (this.esVacia()) {
+			this.info = elem;
+			this.cantidad++;
+			return true;
+		}
+		ListaEnlazada nuevo = new ListaEnlazada(elem);
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		while(aux.siguiente != null) {
+			aux = aux.siguiente;
+		}
+		aux.siguiente = nuevo;
+		this.cantidad++;
+		return true;
 	}
-	
-	//METODOS
-	
-}
+		
+	public boolean agregarTodos(Lista<T> otraLista) {
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		while(aux.siguiente != null) {
+			aux = aux.siguiente;
+		}
+		otraLista = (ListaEnlazada)otraLista;
+		aux.siguiente = (ListaEnlazada)otraLista;
+		this.cantidad = this.cantidad+otraLista.elementos();
+		
+		return true;
+	}
 
+	public boolean insertar(T elem, int indice) {
+		if (indice > cantidad || indice < 0) {
+			return false;
+		}
+		ListaEnlazada aux1 = new ListaEnlazada();
+		ListaEnlazada aux2 = new ListaEnlazada();
+		aux1 = aux2 = this;
+		for (int i = 0; i < indice-1; i++) {
+			aux1 = aux1.siguiente;
+		}
+		for (int i = 0; i < indice; i++) {
+			aux2 = aux2.siguiente;
+		}
+		ListaEnlazada nuevo = new ListaEnlazada(elem);
+		aux1.siguiente = nuevo;
+		nuevo.siguiente = aux2;
+		return true;
+	}
+	
+	public T eliminar(int indice) {
+		ListaEnlazada aux1 = new ListaEnlazada();
+		ListaEnlazada aux2 = new ListaEnlazada();
+		aux1 = aux2 = this;
+		for (int i = 0; i < indice-1; i++) {
+			aux1 = aux1.siguiente;
+		}
+		for (int i = 0; i <= indice; i++) {
+			aux2 = aux2.siguiente;
+		}
+		T info = (T)aux1.siguiente.info;
+		aux1.siguiente = aux2;
+		return info;
+	}
+
+	public T obtener(int indice) {
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		for (int i = 0; i < indice; i++) {
+			aux = aux.siguiente;
+		}
+		return (T)aux.info;
+	}
+
+	public ListaEnlazada<T> subLista(int desdeInd, int hastaInd){
+		ListaEnlazada aux1 = new ListaEnlazada();
+		ListaEnlazada aux2 = new ListaEnlazada();
+		aux1 = aux2 = this;
+		
+		for (int i = 0; i < desdeInd; i++) {
+			aux1 = aux1.siguiente;
+		}
+		for (int i = 0; i < hastaInd; i++) {
+			aux2 = aux2.siguiente;
+		}
+		
+		ListaEnlazada nuevo = new ListaEnlazada(aux1.info);
+		while (aux1 != aux2) {
+			aux1 = aux1.siguiente;
+			nuevo.agregar(aux1.info);
+		}
+		
+		return nuevo;
+	}
+
+	public boolean contiene(T elem) {
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		while (aux != null) {
+			if (elem.equals(aux.info)) {
+				return true;
+			}
+			aux = aux.siguiente;
+		}
+		return false;
+	}
+
+	public void vaciar() {
+		this.info = null;
+		this.siguiente = null;
+		this.cantidad = 0;
+	}
+	
+	public int elementos() {
+		return this.cantidad;
+	}
+
+	public boolean esVacia() {
+		return this.cantidad == 0;
+	}
+
+	public boolean repOK() {
+		if (this.cantidad < 0)
+			return false;
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		while (aux != null) {
+			if (aux.info == null) {
+				return false;
+			}
+			aux = aux.siguiente;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder cadena = new StringBuilder();
+		cadena.append("[");
+		ListaEnlazada aux = new ListaEnlazada();
+		aux = this;
+		while (aux != null) {
+			if(aux.siguiente == null) {
+				cadena.append(String.valueOf(aux.info));
+				aux = aux.siguiente;
+			}else {
+				cadena.append(String.valueOf(aux.info+", "));
+				aux = aux.siguiente;
+			}
+		}
+		cadena.append("]");
+		return cadena.toString();
+	}
+	
+	@Override
+	public boolean equals(Object otro) {
+		if (otro == this)
+			return true;
+		if (otro == null & this == null)
+			return true;
+		ListaEnlazada aux = (ListaEnlazada)otro;
+		if (this.cantidad == aux.cantidad) {
+			while (aux != null) {
+				if (this.info != aux.info) {
+					return false;
+				}
+				aux = aux.siguiente;
+			}
+			return true;
+		}
+		if (this.cantidad != aux.cantidad)
+			return false;
+		return true;
+	}
+}
--- Practico2/src/lista/MainListaEnlazada.java
+++ Practico2/src/lista/MainListaEnlazada.java
@@ -1,12 +1,62 @@
 package lista;
+import lista.ListaEnlazada;
 
 public class MainListaEnlazada {
 
 	public static void main(String[] args) {
+		
 		ListaEnlazada lista1 = new ListaEnlazada();
+		ListaEnlazada lista2 = new ListaEnlazada();
+		ListaEnlazada lista3 = new ListaEnlazada();
 		
 		//AGREGAR
-		lista1.agregar(2);
-		System.out.println("La lista 1 esta compuesta por: "+lista1);
+		for (int i = 20; i < 30; i++) {
+			lista1.agregar(i);
+		}
+		System.out.println("Lista 1: "+lista1);
+		for (int i = 30; i < 40; i++) {
+			lista2.agregar(i);
+		}
+		System.out.println("Lista 2: "+lista2);		
+		for (int i = 40; i < 45; i++) {
+			lista3.agregar(i);
+		}
+		System.out.println("Lista 3: "+lista3);	
+		
+		//AGREGARTODOS
+		lista2.agregarTodos(lista3);
+		System.out.println("Conbimación entre lista 2 con lista 3: "+lista2);
+		
+		//INSERTAR
+		lista1.insertar(23.14, 4);
+		System.out.println("Inserción entre 23 y 24: "+lista1);
+		
+		//ELIMINAR
+		lista1.eliminar(4);
+		System.out.println("Eliminación de la inserción: "+lista1);
+		
+		//OBTENER
+		System.out.println("Tercer elemento de la lista 3: "+lista3.obtener(2));
+		
+		//SUBLISTA
+		ListaEnlazada lista4 = lista2.subLista(5, 9);
+		System.out.println("Nueva lista con la mitad de elementos de la lista 2, lista 4: "+lista4);
+		
+		//CONTIENE
+		System.out.println("¿La lista 4 contiene el 30? "+lista4.contiene(30));
+		
+		//ELEMENTOS
+		System.out.println("Cantidad de elementos lista 4: "+lista4.elementos());
+		
+		//VACIAR Y ESVACIA
+		lista4.vaciar();
+		System.out.println("La lista 4 fue vaciada, ¿Esto es correcto?: "+lista4.esVacia());
+		
+		//REPOK
+		System.out.println("¿La lista 1 es correcta en su estructura?: "+lista1.repOK());
+		System.out.println("¿La lista 4 es correcta en su estructura?: "+lista4.repOK());
+		
+		//EQUALS
+		System.out.println("¿La lista 1 y 2 son equivalentes?: "+lista1.equals(lista2));
 	}
 }
